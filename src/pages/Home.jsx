d176681390d8cbec98e356002c702558ec563ac4
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-10-03 18:00:07 +0700

    create navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import "../styles/style.css";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
