20c2eaf86f1930ca21df0b4cfb55d80f001e3b29
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-10-04 11:32:56 +0700

    update styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,31 @@
 import React from "react";
-import "../styles/style.css"
+import "../styles/style.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import logo from '../assets/logo.svg';
+import logo from "../assets/logo.svg";
 
 const NavbarMovies = () => {
   return (
-    <Navbar className="Navbar-Movies container-fluid" expand="lg">
-      <Container className="Navbar-Nav">
-        <Navbar.Brand href="#home" className="LogoMovies">
-          <img src={logo} alt="Logo Movies" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className="Navbar-List">
-          <Nav className="gap-2">
-            <a href="#home" className="List">HOME</a>
-            <a href="#favorite" className="List">FAVORITES</a>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar className="Navbar-Movies container-fluid" expand="lg">
+        <Container className="Navbar-Nav">
+          <Navbar.Brand href="#home" className="LogoMovies">
+            <img src={logo} alt="Logo Movies" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-white" />
+          <Navbar.Collapse className="Navbar-List">
+            <Nav className="gap-2">
+              <a href="#home" className="List">
+                HOME
+              </a>
+              <a href="#favorite" className="List">
+                FAVORITES
+              </a>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <hr className="container-fluid text-white"/>
+    </>
   );
 };
 
