35fbd1bae4acd1ff70f0c3cb4b9d338ceb702c14
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-10-04 12:16:37 +0700

    create favorite page

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/style.css';
-import Home from './pages/Home';
+// import Home from './pages/Home';
+import Favorite from './pages/Favorite'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Favorite />
   </React.StrictMode>
 );
