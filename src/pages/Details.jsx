9b35149afe9de6cdea409fa73df502a363141427
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-10-04 12:34:13 +0700

    update detail page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "../styles/style.css";
+import { Row, Col } from "react-bootstrap";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Poster from "../assets/poster-onepiece.png"
 
 const Details = () => {
   return (
@@ -11,10 +13,21 @@ const Details = () => {
         <header className="Header-Movies container">
           <h2>DETAILS MOVIES</h2>
         </header>
-        <div>
-          
+        <div className="container Body Movie-Body">
+          <Row>
+            <Col className="Img-Movie" xxl={5} lg={6} md={12}>
+              <img src={Poster} alt="MovieImage" />
+            </Col>
+            <Col className="Movie-Detail" xxl={7} lg={6} md={12}>
+              <h2>One Piece: RED</h2>
+              <p>Untuk pertama kalinya, Uta - penyanyi paling dicintai akan mengungkapkan dirinya kepada dunia di konser langsungnya.</p>
+              <p>Rating Movie: 7.5 / 10</p>
+              <p>Popularity: 12.953 billion yen or US$94.04 million</p>
+              <p>Release Date: 21 September 2022</p>
+            </Col>
+          </Row>
         </div>
-        <Footer/>
+        <Footer />
       </div>
     </>
   );
