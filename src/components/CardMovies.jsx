53275f93fb24d86888614ca247ae7066a8b1a450
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-10-05 20:26:46 +0700

    data fecthing now playing movies home page

--- src/components/CardMovies.jsx
+++ src/components/CardMovies.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import "../styles/style.css";
-import Poster from "../assets/poster-onepiece.png"
+// import Poster from "../assets/poster-onepiece.png"
 import { Card, CardGroup, Row, Col } from "react-bootstrap";
 
-const CardMovies = () => {
+const CardMovies = (props) => {
   return (
     <>
       <CardGroup className="Card-Movies">
         <Row className="Card-List">
           <Col className="List-Movies">
             <Card className="Card-Body">
-              <Card.Img src={Poster} />
+              <Card.Img src={props.src} />
               <Card.Body className="TitleMovies">
                 <Card.Link>
-                  <h3 style={{color: "white"}}>One Piece: RED</h3>
+                  <h3 style={{color: "white"}}>{props.title}</h3>
                 </Card.Link>
                 <button
                   className="Btn-Favorite"
